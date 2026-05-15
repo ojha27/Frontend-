dae3c84c49a7012782664dbac52b7d5b78691fa6
Amrita Ojha <amrita@topsinfosolutions.com>
2026-05-15 21:59:03 +0530

    fix the api issue

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,16 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL:"https://backend-qwbj.onrender.com",
+  baseURL: "https://backend-qwbj.onrender.com",
 });
 
-export const getTasks = () => API.get("/tasks");
-export const createTask = (data) => API.post("/tasks", data);
-export const updateTask = (id) => API.put(`/tasks/${id}`);
-export const deleteTask = (id) => API.delete(`/tasks/${id}`);
+export const getTasks = () => API.get("/api/tasks");
+
+export const createTask = (data) =>
+  API.post("/api/tasks", data);
+
+export const updateTask = (id) =>
+  API.put(`/api/tasks/${id}`);
+
+export const deleteTask = (id) =>
+  API.delete(`/api/tasks/${id}`);
